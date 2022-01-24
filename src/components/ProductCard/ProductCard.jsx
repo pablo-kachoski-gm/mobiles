e9b0183d8b2a071292c05edea8834799dfd485c1
pablo-kachoski-gm <pkachoski@gmail.com>
2022-01-24 03:26:40 -0300

    feat(Products): add onClick navigation to productDetails page

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,12 +1,12 @@
 import { useFormatNumber } from 'react-intl-hooks';
 import './productCard.css';
 
-const ProductCard = ({ product: { model, brand, imgUrl, price } }) => {
+const ProductCard = ({ product: { model, brand, imgUrl, price }, onClick }) => {
   const t = useFormatNumber();
 
   const formmatedPrice = t(price, { style: 'currency', currency: 'EUR' });
   return (
-    <span className="product-card">
+    <span className="product-card" onClick={onClick}>
       <img src={imgUrl} alt="mobile-phone" />
       <div className="product-card-description">
         <div className="model">{model}</div>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,17 +1,24 @@
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { ProductCard } from '../ProductCard';
 import './products.css';
 
 const Products = () => {
   const { products, loadingProducts } = useSelector((state) => state.productsReducer);
+  const navigate = useNavigate();
 
   if (loadingProducts) return <div className="loader">loading..</div>;
+
+  const onProductClick = (id) => {
+    const navigateTo = ['products', id].join('/');
+    navigate(navigateTo);
+  };
 
   return (
     <div className="products-grid-container">
       {products.map((product) => (
         <div key={product.id} className="grid-item">
-          <ProductCard product={product} />
+          <ProductCard onClick={() => onProductClick(product.id)} product={product} />
         </div>
       ))}
     </div>
