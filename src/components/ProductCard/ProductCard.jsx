537bad71aa26785b7c0910fe5fe69fbe1a803213
pablo-kachoski-gm <pkachoski@gmail.com>
2022-01-24 03:11:01 -0300

    refactor(ProductCard): add Product card component

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,20 @@
+import { useFormatNumber } from 'react-intl-hooks';
+import './productCard.css';
+
+const ProductCard = ({ product: { model, brand, imgUrl, price } }) => {
+  const t = useFormatNumber();
+
+  const formmatedPrice = t(price, { style: 'currency', currency: 'EUR' });
+  return (
+    <span className="product-card">
+      <img src={imgUrl} alt="mobile-phone" />
+      <div className="product-card-description">
+        <div className="model">{model}</div>
+        <div className="brand">{brand}</div>
+        <div className="price">{formmatedPrice}</div>
+      </div>
+    </span>
+  );
+};
+
+export default ProductCard;
