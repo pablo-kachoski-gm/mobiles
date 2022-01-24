09e6e41b6f96809ef463468e2293a97ef7f73cbf
pablo-kachoski-gm <pkachoski@gmail.com>
2022-01-24 03:12:26 -0300

    refactor(NotFound): use page container component

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,14 @@
 import { useFormatMessage } from 'react-intl-hooks';
+import PageContainer from '../../components/PageContainer/PageContainer';
 import './notFound.css';
 
-const NotFound = () => {
+const NotFoundPage = () => {
   const t = useFormatMessage();
   const pageTitle = t({ id: 'notFound.title' });
-  return <h2 className="not-found-content">{pageTitle}</h2>;
+  return (
+    <PageContainer>
+      <h2 className="not-found-content">{pageTitle}</h2>
+    </PageContainer>
+  );
 };
-export default NotFound;
+export default NotFoundPage;
