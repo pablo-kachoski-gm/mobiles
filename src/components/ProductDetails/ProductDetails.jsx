7c2f18c969f730377bdeda6b009f0d3273595496
pablo-kachoski-gm <pkachoski@gmail.com>
2022-01-25 00:10:37 -0300

    refactor(Products): modify styles

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,13 +1,17 @@
+import { useFormatMessage } from 'react-intl-hooks';
 import { useSelector } from 'react-redux';
 import { ProductCard } from '../ProductCard';
 import './productDetails.css';
 
 const ProductDetails = () => {
+  const t = useFormatMessage();
+  const loadingText = t({ id: 'data.loading' });
+
   const { productDetails, loadingProductDetails } = useSelector(
     (state) => state.productDetailsReducer
   );
 
-  if (loadingProductDetails) return <div className="loader">loading..</div>;
+  if (loadingProductDetails) return <div className="loader">{loadingText}</div>;
 
   return (
     <div className="product-details-container">
