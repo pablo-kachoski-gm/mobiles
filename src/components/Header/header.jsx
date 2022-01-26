90ffb0f53878dada9d96eca0939674209f71079a
pablo-kachoski-gm <pkachoski@gmail.com>
2022-01-25 23:37:23 -0300

    feat(Header): add header link to main page

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,9 +1,14 @@
 import { useFormatMessage } from 'react-intl-hooks';
+import { Link } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
   const t = useFormatMessage();
   const headerTitle = t({ id: 'app.header.title' });
-  return <header className="App-header">{headerTitle}</header>;
+  return (
+    <header className="App-header">
+      <Link to="/">{headerTitle}</Link>
+    </header>
+  );
 };
 export default Header;
